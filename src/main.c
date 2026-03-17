12944aed939ce8f9c41c973ec1cb07d27d5dd061
finnyjh <68309914+how2fps@users.noreply.github.com>
2026-03-17 19:47:45 +0800

    .

--- src/main.c
+++ src/main.c
@@ -7,13 +7,19 @@ int main()
        FILE *fp;
 
        /*load equation list from file here*/
+       char *equation;
        int attempts = 0;
        int game_won = 0;
        int max_guess = 6;
        int line_count = 0;
+       int random_line;
        /*char equation[9];
        GameState game;*/
        char line[100];
+       int i;
+       int len;
+
+       srand(time(NULL));
        fp = read_file("equations.txt");
        while (fgets(line, sizeof(line), fp))
        {
@@ -22,8 +28,35 @@ int main()
 
        printf("%d", line_count);
 
+       random_line = rand() % line_count;
+       rewind(fp);
+
+       for (i = 0; i <= random_line; i++)
+       {
+              if (fgets(line, sizeof(line), fp) == NULL)
+              {
+                     break;
+              }
+       }
+       fclose(fp);
+       len = strlen(line);
+       if (len > 0 && line[len - 1] == '\n')
+       {
+              line[len - 1] = '\0';
+              len--;
+       }
+       equation = (char *)malloc(sizeof(char) * (len + 1));
+       if (equation == NULL)
+       {
+              return 1;
+       }
+       strncpy(equation, line, len);
+       equation[len] = '\0'; /* we get equation here, now we scanf */
+
        while (attempts < max_guess && game_won != 1)
        {
+
+              
               /*game logic*/
 
               attempts++;
