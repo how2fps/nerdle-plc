c8bb13430155d5cf2bc49e3acfa2c1cf9de7a9fa
how2fps <hearthstonesigh@gmail.com>
2026-03-18 11:51:20 +0800

    .

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 #include "file_reader.h"
 #include "game_logic.h"
+
+#define MAX_ANSWER_SIZE 8
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define RESET "\033[0m"
 
 int main()
 {
@@ -8,6 +17,7 @@ int main()
 
        /*load equation list from file here*/
        char *equation;
+       char *answer;
        int attempts = 0;
        int game_won = 0;
        int max_guess = 6;
@@ -26,8 +36,6 @@ int main()
               line_count++;
        }
 
-       printf("%d", line_count);
-
        random_line = rand() % line_count;
        rewind(fp);
 
@@ -39,30 +47,44 @@ int main()
               }
        }
        fclose(fp);
+
        len = strlen(line);
        if (len > 0 && line[len - 1] == '\n')
        {
               line[len - 1] = '\0';
               len--;
        }
+
        equation = (char *)malloc(sizeof(char) * (len + 1));
        if (equation == NULL)
        {
               return 1;
        }
-       strncpy(equation, line, len);
-       equation[len] = '\0'; /* we get equation here, now we scanf */
 
+       strncpy(equation, line, len);
+       equation[len] = '\0';
+
+       answer = (char *)malloc(sizeof(char) * (MAX_ANSWER_SIZE + 1));
+       if (answer == NULL)
+       {
+              return 1;
+       }
+       printf("%s\n", equation);
+       printf("Start guessing!\n");
        while (attempts < max_guess && game_won != 1)
        {
-
-              
-              /*game logic*/
+              scanf(" %8s", answer);
+              if (strncmp(answer, equation, 8) == 0)
+              {
+                     printf("You got the answer!");
+                     break;
+              };
 
               attempts++;
        }
 
        if (game_won == 1)
+
        {
               /* game win logic */
        }
