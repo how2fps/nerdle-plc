88d7e53b01c0f5a73938e5f3fc24122091b7f472
finnyjh <68309914+how2fps@users.noreply.github.com>
2026-04-01 15:24:40 +0800

    added tokenizer for equation

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,7 +1,45 @@
-
 #include "tokenizer.h"
+#include <ctype.h>
 
-char *tokenize_expression(const char *expression)
+char *tokenize_expression(const char *expression, Token tokens[EQUATION_LEN + 1])
 {
+       char *ptr = expression;
        int i = 0;
+       int char_idx;
+       while (*ptr != '\0' && i < EQUATION_LEN + 1)
+       {
+              if (isdigit(*ptr))
+              {
+                     char_idx = 0;
+                     tokens[i].is_number = 1;
+                     while (isdigit(*ptr))
+                     {
+                            if (char_idx < EQUATION_LEN)
+                            {
+                                   tokens[i].text[char_idx] = *ptr;
+                                   char_idx++;
+                            }
+                            ptr++;
+                     }
+                     tokens[i].text[char_idx] = '\0';
+                     i++;
+              }
+              else if (isspace(*ptr))
+              {
+                     ptr++;
+              }
+              else
+              {
+                     tokens[i].is_number = 0;
+                     tokens[i].text[0] = *ptr;
+                     tokens[i].text[1] = '\0';
+                     i++;
+                     ptr++;
+              }
+       }
+       if (i <= EQUATION_LEN)
+       {
+              tokens[i].text[0] = '\0';
+       }
+       return (char *)expression;
 }
--- src/tokenizer.h
+++ src/tokenizer.h
@@ -1,7 +1,12 @@
 #ifndef TOKENIZER_H
 #define TOKENIZER_H
 #define EQUATION_LEN 8
+typedef struct
+{
+       char text[EQUATION_LEN];
+       int is_number;
+} Token;
 
-char *tokenize_expression(const char *expression);
+char *tokenize_expression(const char *expression, Token tokens[EQUATION_LEN + 1]);
 
 #endif
