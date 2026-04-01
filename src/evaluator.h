8e492a747a81b333458d772fe51c75223b4e4484
finnyjh <68309914+how2fps@users.noreply.github.com>
2026-04-01 18:52:15 +0800

    .

--- src/evaluator.h
+++ src/evaluator.h
@@ -2,6 +2,12 @@
 #define EVALUATOR_H
 #include "tokenizer.h"
 
+void process_line(char *line);
+
+void infix_to_postfix(Token infix[EQUATION_LEN + 1], Token postfix[EQUATION_LEN + 1]);
+
 int evaluate_expression(Token tokens[EQUATION_LEN + 1]);
+
+int precedence(char op);
 
 #endif
