ff2a43d4ebedd375b41e8e18b187b8264b80bd9e
how2fps <hearthstonesigh@gmail.com>
2026-04-15 11:16:08 +0800

    fixed a few small stufff

--- src/input.c
+++ src/input.c
@@ -5,13 +5,13 @@
 
 int custom_getch(void)
 {
-    struct termios oldt, newt;
-        int ch;
-        tcgetattr(STDIN_FILENO, &oldt);
-        newt = oldt;
-        newt.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-        ch = getchar();
-        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-        return ch;
+       struct termios oldt, newt;
+       int ch;
+       tcgetattr(STDIN_FILENO, &oldt);
+       newt = oldt;
+       newt.c_lflag &= ~(ICANON | ECHO);
+       tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+       ch = getchar();
+       tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+       return ch;
 }
